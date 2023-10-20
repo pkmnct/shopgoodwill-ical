e1e6f666156e9c27e4febcf7fa14e585db58da40
George Walker <george@georgewwalker.com>
2023-10-20 19:23:50 -0400

    Improve iCal server

--- src/ical.ts
+++ src/ical.ts
@@ -4,28 +4,42 @@ import ical from "ical-generator";
 import http from "node:http";
 
 export const ServeIcal = async () => {
+  if (!process.env.ICAL_UNIQUE_URL) {
+    throw new Error("You must set ICAL_UNIQUE_URL");
+  }
+
+  const port = process.env.ICAL_PORT ? parseInt(process.env.ICAL_PORT) : 3000;
+  const url = process.env.ICAL_URL || "127.0.0.1";
+
   http
     .createServer((req, res) => {
-      (async () => {
-        console.log("Incoming request");
-        const favorites: ShopgoodwillFavorite[] = await GetFavorites();
-        const calendar = ical({
-          name: `${process.env.SHOPGOODWILL_USERNAME} Shopgoodwill Favorites`,
-        });
-
-        favorites.forEach((favorite) => {
-          calendar.createEvent({
-            start: favorite.endTime,
-            end: favorite.endTime,
-            summary: favorite.title,
-            description: `This item is ending on Goodwill at this time. Make sure to set a notification. The item is here: https://shopgoodwill.com/item/${favorite.itemId}`,
+      if (req.url === "/" + process.env.ICAL_UNIQUE_URL) {
+        console.log(`Valid incoming request, getting favorites...`);
+        (async () => {
+          const favorites: ShopgoodwillFavorite[] = await GetFavorites();
+          const calendar = ical({
+            name: `${process.env.SHOPGOODWILL_USERNAME} Shopgoodwill Favorites`,
           });
-        });
 
-        calendar.serve(res);
-      })();
+          favorites.forEach((favorite) => {
+            calendar.createEvent({
+              start: favorite.endTime,
+              end: favorite.endTime,
+              summary: favorite.title,
+              description: `This item is ending on Goodwill at this time. Make sure to set a notification. The item is here: https://shopgoodwill.com/item/${favorite.itemId}`,
+            });
+          });
+
+          calendar.serve(res);
+        })();
+      } else {
+        console.log("Invalid incoming request", req.url, req.headers);
+        res.destroy();
+      }
     })
-    .listen(3000, "127.0.0.1", () => {
-      console.log("Server running at http://127.0.0.1:3000/");
+    .listen(port, url, () => {
+      console.log(
+        `iCal Feed Served at http://${url}:${port}/${process.env.ICAL_UNIQUE_URL}`
+      );
     });
 };
